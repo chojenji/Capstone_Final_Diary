d8188ec4a6254860b67267c348a2e9bdf983a0f4
chojenji <oouiyty@naver.com>
2023-11-24 20:34:07 +0900

    로딩중 ui수정

--- app/src/main/java/com/example/capstonefinaldiary/Loading.java
+++ app/src/main/java/com/example/capstonefinaldiary/Loading.java
@@ -1,6 +1,7 @@
 package com.example.capstonefinaldiary;
 import android.app.Activity;
 import android.app.Dialog;
+import android.graphics.drawable.ColorDrawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
@@ -17,13 +18,14 @@ public class Loading{
 
     public void startLoadingDialog() {
         dialog = new Dialog(activity);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT)); // Set the dialog window background to transparent
         LayoutInflater inflater = activity.getLayoutInflater();
         View view = inflater.inflate(R.layout.load, null);
         ImageView gifImageView = view.findViewById(R.id.loadingImage);
 
         Glide.with(activity)
                 .asGif()
-                .load(R.drawable.loading)
+                .load(R.drawable.load_final)
                 .into(gifImageView);
 
         dialog.setContentView(view);
